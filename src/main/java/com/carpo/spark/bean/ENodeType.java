eb3fd246dfe10a09b3773a18542f7dff076e3ef1
eliyanfei <eliyanfei@126.com>
2018-02-09 10:01:35 +0800

    加入Node可以连接的下一个节点限制

--- src/main/java/com/carpo/spark/bean/ENodeType.java
+++ src/main/java/com/carpo/spark/bean/ENodeType.java
@@ -6,10 +6,20 @@ package com.carpo.spark.bean;
  * 2018/2/5
  */
 public enum ENodeType {
-    input("HDFS"), output("HDFS"), filter_col("过滤列"), filter_row("过滤行"), map("Key Value"), distinct("去重"), join("合并"), union("联合"), group("分组");
+    input("HDFS", ENodeType.filter_col, ENodeType.filter_row, ENodeType.distinct, ENodeType.map, ENodeType.union),
+    filter_col("过滤列", ENodeType.distinct, ENodeType.union, ENodeType.map, ENodeType.filter_row),
+    filter_row("过滤行", ENodeType.distinct, ENodeType.union, ENodeType.map, ENodeType.filter_col),
+    distinct("去重", ENodeType.group, ENodeType.join, ENodeType.union, ENodeType.map, ENodeType.output),
+    map("Key Value", ENodeType.group, ENodeType.join, ENodeType.distinct, ENodeType.output),
+    join("合并", ENodeType.distinct, ENodeType.group, ENodeType.output),
+    union("联合", ENodeType.distinct, ENodeType.group, ENodeType.output),
+    group("分组", ENodeType.output),
+    output("HDFS");
     public String name;
+    public ENodeType[] nexts;
 
-    private ENodeType(String name) {
+    private ENodeType(String name, ENodeType... nexts) {
         this.name = name;
+        this.nexts = nexts;
     }
 }
